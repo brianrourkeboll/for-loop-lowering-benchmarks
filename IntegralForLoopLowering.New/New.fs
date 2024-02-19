d5adc1f5cc8df3976fa1d3fd78626667d62137fb
Brian Rourke Boll <brianrourkeboll@gmail.com>
2024-02-19 09:50:33 -0500

    Init

---
+++ IntegralForLoopLowering.New/New.fs
@@ -0,0 +1,44 @@
+﻿module IntegralForLoopLowering.New
+
+open System.Runtime.CompilerServices
+
+module UInt32 =
+    [<MethodImpl(MethodImplOptions.NoInlining)>]
+    let ``10u..1u`` () = for _ in 10u..1u do ignore (2. ** 2.)
+
+    [<MethodImpl(MethodImplOptions.NoInlining)>]
+    let ``1u..256u`` () = for _ in 1u..256u do ignore (2. ** 2.)
+
+    [<MethodImpl(MethodImplOptions.NoInlining)>]
+    let ``start..finish`` (start : uint) finish = for _ in start..finish do ignore (2. ** 2.)
+
+    [<MethodImpl(MethodImplOptions.NoInlining)>]
+    let ``10u..2u..1u`` () = for _ in 10u..2u..1u do ignore (2. ** 2.)
+
+    [<MethodImpl(MethodImplOptions.NoInlining)>]
+    let ``1u..2u..256u`` () = for _ in 1u..2u..256u do ignore (2. ** 2.)
+
+    [<MethodImpl(MethodImplOptions.NoInlining)>]
+    let ``start..step..finish`` (start : uint) step finish = for _ in start..step..finish do ignore (2. ** 2.)
+
+module Int64 =
+    [<MethodImpl(MethodImplOptions.NoInlining)>]
+    let ``10L..1L`` () = for _ in 10L..1L do ignore (2. ** 2.)
+
+    [<MethodImpl(MethodImplOptions.NoInlining)>]
+    let ``1L..256L`` () = for _ in 1L..256L do ignore (2. ** 2.)
+
+    [<MethodImpl(MethodImplOptions.NoInlining)>]
+    let ``start..finish`` (start : int64) finish = for _ in start..finish do ignore (2. ** 2.)
+
+    [<MethodImpl(MethodImplOptions.NoInlining)>]
+    let ``10L..2L..1L`` () = for _ in 10L..2L..1L do ignore (2. ** 2.)
+
+    [<MethodImpl(MethodImplOptions.NoInlining)>]
+    let ``10L..-1L..1L`` () = for _ in 10L.. -1L..1L do ignore (2. ** 2.)
+
+    [<MethodImpl(MethodImplOptions.NoInlining)>]
+    let ``1L..2L..256L`` () = for _ in 1L..2L..256L do ignore (2. ** 2.)
+
+    [<MethodImpl(MethodImplOptions.NoInlining)>]
+    let ``start..step..finish`` (start : int64) step finish = for _ in start..step..finish do ignore (2. ** 2.)
---
+++ IntegralForLoopLowering/Program.fs
@@ -0,0 +1,69 @@
+﻿module IntegralForLoopLowering.Benchmarks
+
+open BenchmarkDotNet.Attributes
+open BenchmarkDotNet.Configs
+open BenchmarkDotNet.Running
+
+[<MemoryDiagnoser; GroupBenchmarksBy(BenchmarkLogicalGroupRule.ByCategory); CategoriesColumn; HideColumns("Method")>]
+type Benchmarks () =
+    [<Benchmark(Baseline = true); BenchmarkCategory("UInt32", "1", "10u..1u")>]
+    member _.UInt32_Old_10uTo1u () = Old.UInt32.``10u..1u`` ()
+
+    [<Benchmark; BenchmarkCategory("UInt32", "1", "10u..1u")>]
+    member _.UInt32_New_10uTo1u () = New.UInt32.``10u..1u`` ()
+
+    [<Benchmark(Baseline = true); BenchmarkCategory("UInt32", "2", "1u..256u")>]
+    member _.UInt32_Old_1uTo256u () = Old.UInt32.``1u..256u`` ()
+
+    [<Benchmark; BenchmarkCategory("UInt32", "2", "1u..256u")>]
+    member _.UInt32_New_1uTo256u () = New.UInt32.``1u..256u`` ()
+
+    [<Benchmark(Baseline = true); BenchmarkCategory("UInt32", "3", "start..finish (start=1u,finish=65536u)")>]
+    member this.UInt32_Old_Dynamic_1uTo65536u () = Old.UInt32.``start..finish`` 1u 65536u
+
+    [<Benchmark; BenchmarkCategory("UInt32", "3", "start..finish (start=1u,finish=65536u)")>]
+    member this.UInt32_New_Dynamic_1uTo65536u () = New.UInt32.``start..finish`` 1u 65536u
+
+    [<Benchmark(Baseline = true); BenchmarkCategory("UInt32", "4", "1u..2u..256u")>]
+    member _.UInt32_Old_1u2u256u () = Old.UInt32.``1u..2u..256u`` ()
+
+    [<Benchmark; BenchmarkCategory("UInt32", "4", "1u..2u..256u")>]
+    member _.UInt32_New_1u2u256u () = New.UInt32.``1u..2u..256u`` ()
+
+    [<Benchmark(Baseline = true); BenchmarkCategory("UInt32", "5", "start..step..finish (start=1u,step=2u,finish=65536u)")>]
+    member this.UInt32_Old_Dynamic_1u2u65536u () = Old.UInt32.``start..step..finish`` 1u 2u 65536u
+
+    [<Benchmark; BenchmarkCategory("UInt32", "5", "start..step..finish (start=1u,step=2u,finish=65536u)")>]
+    member this.UInt32_New_Dynamic_1u2u65536u () = New.UInt32.``start..step..finish`` 1u 2u 65536u
+
+    [<Benchmark(Baseline = true); BenchmarkCategory("Int64", "1", "10L..1L")>]
+    member _.Int64_Old_10LTo1L () = Old.Int64.``10L..1L`` ()
+
+    [<Benchmark; BenchmarkCategory("Int64", "1", "10L..1L")>]
+    member _.Int64_New_10LTo1L () = New.Int64.``10L..1L`` ()
+
+    [<Benchmark(Baseline = true); BenchmarkCategory("Int64", "2", "1L..256L")>]
+    member _.Int64_Old_1LTo256L () = Old.Int64.``1L..256L`` ()
+
+    [<Benchmark; BenchmarkCategory("Int64", "2", "1L..256L")>]
+    member _.Int64_New_1LTo256L () = New.Int64.``1L..256L`` ()
+
+    [<Benchmark(Baseline = true); BenchmarkCategory("Int64", "3", "start..finish (start=1L,finish=65536L)")>]
+    member this.Int64_Old_Dynamic_1LTo65536L () = Old.Int64.``start..finish`` 1L 65536L
+
+    [<Benchmark; BenchmarkCategory("Int64", "3", "start..finish (start=1L,finish=65536L)")>]
+    member this.Int64_New_Dynamic_1LTo65536L () = New.Int64.``start..finish`` 1L 65536L
+
+    [<Benchmark(Baseline = true); BenchmarkCategory("Int64", "4", "1L..2L..256L")>]
+    member _.Int64_Old_1L2L256L () = Old.Int64.``1L..2L..256L`` ()
+
+    [<Benchmark; BenchmarkCategory("Int64", "4", "1L..2L..256L")>]
+    member _.Int64_New_1L2L256L () = New.Int64.``1L..2L..256L`` ()
+
+    [<Benchmark(Baseline = true); BenchmarkCategory("Int64", "5", "start..step..finish (start=1L,step=2L,finish=65536L)")>]
+    member this.Int64_Old_Dynamic_1L2L65536L () = Old.Int64.``start..step..finish`` 1L 2L 65536L
+
+    [<Benchmark; BenchmarkCategory("Int64", "5", "start..step..finish (start=1L,step=2L,finish=65536L)")>]
+    member this.Int64_New_Dynamic_1L2L65536L () = New.Int64.``start..step..finish`` 1L 2L 65536L
+
+ignore (BenchmarkRunner.Run<Benchmarks> ())
